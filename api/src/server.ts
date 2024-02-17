a64caf1e93221b715176c8229810eee499bc745f
DSenpai <gogniashvili.amz@gmail.com>
2024-02-17 17:32:47 +0400

    added test serving

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,11 +7,13 @@ const app: Express = express();
 const port = process.env.PORT;
 
 export const runServer = async () => {
-	app.get("/", (req, res) => {
-		res.send("Hello");
-	});
+	app.use("/", express.static("../ui/build"));
+
+	// app.get("/", (req, res) => {
+	// 	res.send("Hello");
+	// });
 
 	app.listen(port, () => {
-		console.log(`[server]: Server is running at http://localhost:${port}`);
+		console.log(`[server]: Server is running at http://localhost:${"../ui/"}`);
 	});
 };
